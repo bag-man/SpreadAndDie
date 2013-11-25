79af6c7a3374a3a8ea5501155546da970c966c4f
bag-man <garland.owen@gmail.com>
2013-11-25 16:46:29 +0000

    Refactoring

--- MainProgram.java
+++ MainProgram.java
@@ -4,28 +4,43 @@ public class MainProgram
 {
   public static void main(String[] args) 
   {
-    //Get number of reigons for board from user
     System.out.print("Enter the number of regions on the board: ");
     int regions = Keyboard.readInt();
 
-    //Create and display board
     Board bd = new Board(regions);
-    bd.updatePlayer();
+    Player pl = new Player();
+
+    pl.updatePlayer();
     bd.printBoard();
 
-    //Get the disease starting point from user
     System.out.println("\nThis is the board, you are the P, enter the co-ordiantes for where the Disease starts.");
     System.out.print("X: ");
     int diseaseX = Keyboard.readInt();
     System.out.print("Y: ");
     int diseaseY = Keyboard.readInt();
 
-    //Place disease
     Disease ick = new Disease();
     ick.createDisease(diseaseX, diseaseY);
     bd.printBoard();
 
-    Game game = new Game(bd);
+    System.out.println("Press enter to move the player randomly");
+    while(bd.endGame() != true)
+    {
+      pl.updatePlayer();
+      ick.updateDisease();
+      bd.printBoard();
+      new Scanner(System.in).nextLine();
+    }
+  }
     
+  public boolean endGame() 
+  { 
+    if(gameOver == true)
+    {
+      System.out.println("Game over!");
+      return true;
+    } else {
+      return false;
+    }
   }
 }
