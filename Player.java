921f53c8f5d962c1829c28a29ae510effc2b83c0
Owen Garland <Garland.owen@gmail.com>
2013-11-28 16:04:32 +0000

    Nearly, and now I have a lecture and an exam!

--- Player.java
+++ Player.java
@@ -5,6 +5,7 @@ public class Player
   private int playerYposition, playerXposition;
   private int diseaseYposition, diseaseXposition;
   private int prevPlayerYposition, prevPlayerXposition;
+  private boolean corner = false;
   private Board bd;
 
   Player(Board board) 
@@ -86,7 +87,9 @@ public class Player
       {
 	prevPlayerYposition = playerYposition;
 	prevPlayerXposition = playerXposition;
-      } 
+      } else {
+	corner = true;
+      }	
 
       runAway();
     } 
@@ -96,7 +99,12 @@ public class Player
       //Randomly move or change neighboring reigons
     }
 
-    bd.setBoard(prevPlayerYposition, prevPlayerXposition, bd.getBoardPositions(prevPlayerYposition, prevPlayerXposition));
+    if(corner == true)
+    {
+      bd.setBoard(prevPlayerYposition, prevPlayerXposition, bd.getBoard(prevPlayerYposition, prevPlayerXposition));
+    } else {
+      bd.setBoard(prevPlayerYposition, prevPlayerXposition, bd.getBoardPositions(prevPlayerYposition, prevPlayerXposition));
+    }
 
     if(bd.getBoard(playerYposition, playerXposition) == bd.D)
     {
